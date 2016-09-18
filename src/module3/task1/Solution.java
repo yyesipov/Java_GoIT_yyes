1840bb2bf05d1f81cb0906f8445a81ae074c3f44
Ivan Vasilyev <viv.dnp@gmail.com>
2016-09-18 23:51:52 +0300

    Add module3 to result after merges

---
+++ src/module3/task1/Solution.java
@@ -0,0 +1,15 @@
+package module3.task1;
+
+public class Solution {
+    public static void main(String[] args){
+        Animal walk = new Animal();
+        walk.walk();
+        Bird fly = new Bird();
+        fly.fly();
+        Bird sing = new Bird();
+        sing.sing();
+        Bird imBird = new Bird();
+        imBird.imBird();
+
+    }
+}
---
+++ src/module3/task2/Main.java
@@ -0,0 +1,11 @@
+package module3.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic sum = new Arithmetic(5, 23);
+        sum.add();
+        Adder w = new Adder(5, 1, true);
+        w.compare();
+
+    }
+}
