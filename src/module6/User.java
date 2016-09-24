f264d5a73becae65d4eec95235a380bb586d50d7
yyesipoov <yyesipov@gmail.com>
2016-09-24 22:31:14 +0300

    Add task4 from module6

---
+++ src/module6/User.java
@@ -0,0 +1,18 @@
+package module6;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+}
